385f5beab58d733821f02b2f0c273e4094bf0552
Henrique Santos <henrique.260204@bento.local>
2022-05-11 10:01:11 -0300

    inicio da criação das classes

---
+++ H.back/cardapio.php
@@ -0,0 +1,81 @@
+<?php
+/*n sei como que vaifunciona, mas acho que teria que 
+extender essas metodos do cardapio para as outras classes
+
+*/
+
+class cardapio 
+{
+    private data;
+    private tipo;
+     
+    
+    
+    
+    
+    
+    
+    function  incluir()
+    {
+
+    }
+    function  alterar()
+    {
+
+    }
+    function  excluir()
+    {
+
+    }
+
+}
+
+class items extends ingredientees
+{
+    
+}
+
+class ingredientes 
+{
+    private descricao;
+    private calorias;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
