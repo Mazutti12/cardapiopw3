5fe05164a6f6a14ee746cde75cf63f3cc19c2fc1
Henrique Santos <henrique.260204@bento.local>
2022-05-13 17:24:48 -0300

    classes do cardapio

--- backend/classCardapio.php
+++ backend/classCardapio.php
@@ -2,14 +2,58 @@
 
 class Cardapio 
 {
-    public $data;
-    private $type;
+    private $id;
+    private $data;
+    private $tipo;
+   
+}
+
+
+class Refeicao 
+{
+    private $id;
+    private $descricao;
+    
+
+    function SomaCalorias()
+    {
+        $ingrediente = new Ingrediente();
+        foreach ($ingrediente as $calorias)
+        {
+            $caloriasFinais += $calorias->$this->caloriasIngredientes;
+        }
+    }
+
+    
+
+
+}
+
+class Ingredientes 
+{
+    private $ingrediente = [];
+    private $caloriasIngrediente;
+    
+   
+
+    function setIngrediente($ingrediente,$calorias)
+    {
+        $this->ingrediente =  ['nome' => $ingrediente,'calorias' => $calorias];
+    }
+
+    
 
 }
 
 
+$
+
+//ta dando um erro n sei oq é ta dando como variavel indefinida
+$ingrediente = new Ingredientes();
+$ingrediente->setIngrediente('massa','450kcal');
 
 
+var_dump($ingrediente);
 
 
 
