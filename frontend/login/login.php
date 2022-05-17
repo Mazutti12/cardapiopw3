f2e5bf2a271c3450f8f8bab244b268944e89c706
Bernardo Marquetti <104903668+ber0604@users.noreply.github.com>
2022-05-17 09:54:49 -0300

    Update login.php

--- frontend/login/login.php
+++ frontend/login/login.php
@@ -1,24 +1,45 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style/style.css">
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<form method="post" action="" name="signin-form">
-  <div class="form-element">
-    <label>Username</label>
-    <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-  </div>
-  <div class="form-element">
-    <label>Password</label>
-    <input type="password" name="password" required />
-  </div>
-  <button type="submit" name="login" value="login">Log In</button>
-</form>
+
+    <div class="col-sm-5 container pt-5">
+       
+        <form action="index.php" method="POST" class="was-validated " >
+          
+            <div class="mb-3 mt-3" >
+                <h1>Cardápio</h1>
+            </div>
+            <div class="mb-3 mt-3" >
+                <label for="email">Login:</label>
+                <input type="name" class="form-control" id="email" placeholder="Entre com seu login" name="email" required>
+                <div class="valid-feedback">Válido.</div>
+                <div class="invalid-feedback">Inválido.</div>
+            </div>
+            <div class="mb-3">
+                <label for="senha">Senha:</label>
+                <input type="password" class="form-control" id="senha" placeholder="Entre com sua senha" name="senha" required>
+                <div class="valid-feedback">Válido.</div>
+                <div class="invalid-feedback">Inválido.</div>
+            </div>
+           
+            <div class="botao">
+                <button type="submit" class="btn btn-primary">Entrar</button>
+            </div>
+
+        </form>
+    </div>
+
 </body>
+
 </html>
 
